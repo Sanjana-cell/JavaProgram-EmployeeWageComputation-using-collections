2fc83b831f7295393f49dafd9aba56d1c8974f10
Sanjana K R <sanjanakr97@gmail.com>
2020-07-18 10:59:39 +0530

    Refactor: modified code to calculate daily wages for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -9,29 +9,40 @@ public class EmpWageComputation {
 	final static int WAGE_PER_HOUR=20; //Assigned wage per hour as 20
 	final static int FULL_TIME=2; //Assigned full time with 2
 	final static int PART_TIME=1; //Assigned part time with 1
+	final static int MAX_WORKING_DAYS=20; //Assigned maximum working days in month with 20
 	static int dailyWage=0;	 //Stores dailyWage of employee
 	static int workHours=0;  //Stores work hour of employee
+	static int totalWorkingDays=1; //Stores total working days of an employee in month
+	static int totalWages=0; //Stores total wages in a month
 	
 	public static void main(String[] args) {
-		int attendence=(int)(Math.random()*3); // Generates 0,1 or 2 using random() and stores the value
+		int attendence; //Stores value of employee attendance 
 		
-		/*Checks if Employee is working full time,part time or absent and store work hours and calculate daily wage of employee
-		  according to work time*/
-		switch(attendence) {
-		case FULL_TIME: System.out.println("Employee is Present Full Time");
+		
+		//Continues till totalWorkings days reached maximum working days
+		while(totalWorkingDays<=MAX_WORKING_DAYS) {
+			attendence=(int)(Math.random()*3); // Generates 0 or 1 using random() and stores the value
+			
+			/*Checks if Employee is working full time,part time or absent and store work hours and calculate daily wage of employee
+		  		according to work time*/
+			switch(attendence) {
+			case FULL_TIME: System.out.println("Employee is Present Full Time");
 						workHours=8;
 						break;
 						
-		case PART_TIME: System.out.println("Employee is Present Part Time");
+			case PART_TIME: System.out.println("Employee is Present Part Time");
 						workHours=4;
 						break;
 						
-		default:	System.out.println("Employee is Absent");
+			default:	System.out.println("Employee is Absent");
 		
+			}
+			dailyWage=workHours*WAGE_PER_HOUR;
+			System.out.println("Daily Wage of an Employee on Day "+totalWorkingDays+" is "+dailyWage);
+			totalWages+=dailyWage; //sum of daily wages in month
+			totalWorkingDays++;
 		}
-		dailyWage=workHours*WAGE_PER_HOUR;
-		
-		System.out.println("Daily Wage of an Employee "+dailyWage);
+		System.out.println("Total Wages in a month = "+totalWages);
 
 	}
 
